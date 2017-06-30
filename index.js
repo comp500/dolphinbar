4fdef015ba787c7a9a8c11c50327a6411a2c522f
comp500 <comp500@users.noreply.github.com>
2017-06-30 22:59:17 +0100

    scan for wiimotes

--- index.js
+++ index.js
@@ -4,22 +4,40 @@ const productID = 774; // Wiimote productID
 
 module.exports = {};
 
-module.exports.wiimotes = [];
 class Wiimote extends HID.HID {
-
+	testConnection() {
+		try {
+			this.write([0x15]);
+			return true;
+		} catch (e) {
+			return false;
+		}
+	}
 };
 
-module.exports.getHIDDevices = function() {
+module.exports.getHIDDevices = function () {
 	return HID.devices();
 };
 
-module.exports.getDolphinBarDevices = function() {
+module.exports.getDolphinBarDevices = function () {
 	let devices = HID.devices();
 	let dolphinBar = [];
-	for (var i = 0; i < devices.length; i++) {
+	for (let i = 0; i < devices.length; i++) {
 		if (devices[i].vendorId == vendorID && devices[i].productId == productID) {
 			dolphinBar[devices[i].interface] = devices[i].path;
 		}
 	}
 	return dolphinBar;
+};
+
+module.exports.scanWiimotes = function (timeoutSec) {
+	let pathList = module.exports.getDolphinBarDevices();
+	let wiimoteList = [];
+	for (let i = 0; i < pathList.length; i++) {
+		let wiimote = new Wiimote(pathList[i]);
+		if (wiimote.testConnection()) {
+			wiimoteList.push(wiimote);
+		}
+	}
+	return wiimoteList;
 };
