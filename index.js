40ff1b9547f929024cc45e835490664b31ae3d72
comp500 <comp500@users.noreply.github.com>
2017-06-30 22:25:50 +0100

    add get devices

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const HID = require("node-hid");
+const vendorID = 1406; // Nintendo vendorID
+const productID = 774; // Wiimote productID
 
+module.exports = {};
+
+module.exports.wiimotes = [];
+class Wiimote extends HID.HID {
+
+};
+
+module.exports.getHIDDevices = function() {
+	return HID.devices();
+};
+
+module.exports.getDolphinBarDevices = function() {
+	let devices = HID.devices();
+	let dolphinBar = [];
+	for (var i = 0; i < devices.length; i++) {
+		if (devices[i].vendorId == vendorID && devices[i].productId == productID) {
+			dolphinBar[devices[i].interface] = devices[i].path;
+		}
+	}
+	return dolphinBar;
+};
